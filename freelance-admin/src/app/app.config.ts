8fc5d76c5e6df358b83d7f23d63c478337d57c6a
=Samuel_Navasardyan <=sur00044@gmail.com>
2025-02-25 12:02:13 +0100

    Conexión con Firebase

--- freelance-admin/src/app/app.config.ts
+++ freelance-admin/src/app/app.config.ts
@@ -3,7 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'proyecto-facturas-475d0',
+        appId: '1:372357498281:web:85902b253654419df2b0b8',
+        storageBucket: 'proyecto-facturas-475d0.firebasestorage.app',
+        apiKey: 'AIzaSyDkNbrBIBekGwaUdoGOXIyHDepMhaB5zHQ',
+        authDomain: 'proyecto-facturas-475d0.firebaseapp.com',
+        messagingSenderId: '372357498281',
+        measurementId: 'G-NBWN3ESHLE',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
