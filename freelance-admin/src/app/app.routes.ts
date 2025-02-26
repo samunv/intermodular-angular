c7bfa96d62435aeb3f81f21a48f7f8ccf00f410e
Alejandro Yepes <alejandroyg3@gmail.com>
2025-02-26 11:43:58 +0100

    Diseño y funcionalidad

--- freelance-admin/src/app/app.routes.ts
+++ freelance-admin/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ProyectosComponent } from './proyectos/proyectos.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { ProyectosCrearComponent } from './proyectos-crear/proyectos-crear.component';
+import { FacturaVistaComponent } from './factura-vista/factura-vista.component'; // Importar componente
 
 export const routes: Routes = [
   { path: '', component: ProyectosComponent },
   { path: 'perfil', component: PerfilComponent },
-  { path: 'proyectos-crear', component: ProyectosCrearComponent } // Ajuste en la ruta
+  { path: 'proyectos-crear', component: ProyectosCrearComponent },
+  { path: 'factura-vista', component: FacturaVistaComponent } // Ajuste en la ruta
 ];
