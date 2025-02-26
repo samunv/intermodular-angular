2d28b4a6f2b1dc2b446625def55e20486ca6149e
Jorge <jorgeanton1810@gmail.com>
2025-02-26 10:56:00 +0100

    Creación de componente crear proyecto

--- freelance-admin/src/app/app.routes.ts
+++ freelance-admin/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProyectosComponent } from './proyectos/proyectos.component';
 import { PerfilComponent } from './perfil/perfil.component';
+import { CrearProyectoComponent } from './proyectos-crear/proyectos-crear.component';
 
 export const routes: Routes = [
   { path: '', component: ProyectosComponent },
   { path: 'perfil', component: PerfilComponent },
+  {path: 'crear-proyecto', component: CrearProyectoComponent}
 ];
---
+++ freelance-admin/src/app/proyectos-crear/proyectos-crear.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proyectos-crear',
+  imports: [],
+  templateUrl: './proyectos-crear.component.html',
+  styleUrl: './proyectos-crear.component.css'
+})
+export class ProyectosCrearComponent {
+
+}
