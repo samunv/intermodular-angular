7b78f93856ac22d13a227cd0b0ea09fc206f010e
Jorge <jorgeanton1810@gmail.com>
2025-02-26 10:56:00 +0100

    Creación de componente crear proyecto

--- freelance-admin/src/app/Proyecto.ts
+++ freelance-admin/src/app/Proyecto.ts
@@ -1,8 +1,9 @@
 export interface Proyecto {
   nombre_proyecto: string; // Nombre del proyecto (obligatorio)
   descripcion: string; // Descripción del proyecto
-  estado: 'Planificado' | 'En desarrollo' | 'En progreso' | 'Completado'; // Estado con valores limitados
+  estado: string; // Estado con valores limitados
   tecnologias: string[]; // Lista de tecnologías utilizadas
   foto: string; // SRC de una foto del proyecto
   id_factura: string; // ID generado de la factura
+  presupuesto: number; // Presupuesto del proyecto
 }
