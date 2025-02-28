442579df91afcd8e4a6db202914bcc9765c727e0
Jorge <jorgeanton1810@gmail.com>
2025-02-28 20:48:03 +0100

    Implementación servicio login y componente login

--- freelance-admin/src/app/app.component.ts
+++ freelance-admin/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ProyectosComponent } from './proyectos/proyectos.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterOutlet, RouterLink, ProyectosComponent],
+	imports: [RouterOutlet, RouterLink, ProyectosComponent,FormsModule],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
--- freelance-admin/src/app/app.routes.ts
+++ freelance-admin/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { ProyectosComponent } from './proyectos/proyectos.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { ProyectosCrearComponent } from './proyectos-crear/proyectos-crear.component';
-import { FacturaVistaComponent } from './factura-vista/factura-vista.component'; // Importar componente
+import { FacturaVistaComponent } from './factura-vista/factura-vista.component'; 
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: ProyectosComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirige al login
+  { path: 'login', component: LoginComponent },
   { path: 'perfil', component: PerfilComponent },
+  { path: 'proyectos', component: ProyectosComponent },
   { path: 'proyectos-crear', component: ProyectosCrearComponent },
-  { path: 'factura-vista/:codigo', component: FacturaVistaComponent } //se añade ":codigo" como parámetro dinámico
+  { path: 'factura-vista/:codigo', component: FacturaVistaComponent } 
 ];
