51e28557c5094ce815241786c4794dd9919c1969
Alejandro Yepes <alejandroyg3@gmail.com>
2025-03-03 11:35:43 +0100

    Implementacion del css y html

--- freelance-admin/src/app/registro/registro.component.ts
+++ freelance-admin/src/app/registro/registro.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RegistroService } from '../services/registro.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registro',
+  imports: [FormsModule], // Agregar FormsModule
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.css'],
 })
@@ -11,6 +14,8 @@ export class RegistroComponent {
   email: string = '';
   password: string = '';
   confirmPassword: string = '';
+  logo = "../img/Freelance-admin-logo.png";
+
 
   constructor(private registroService: RegistroService, private router: Router) {}
 
@@ -22,34 +27,33 @@ export class RegistroComponent {
       alert('⚠️ Todos los campos son obligatorios.');
       return;
     }
-
+  
     if (!this.isValidEmail(this.email)) {
       alert('⚠️ Ingresa un correo válido.');
       return;
     }
-
+  
     if (this.password.length < 6) {
       alert('⚠️ La contraseña debe tener al menos 6 caracteres.');
       return;
     }
-
+  
     if (this.password !== this.confirmPassword) {
       alert('⚠️ Las contraseñas no coinciden.');
       return;
     }
-
+  
     try {
       await this.registroService.register(this.email, this.password);
-      alert(' Registro exitoso');
+      alert('✅ Registro exitoso');
       this.router.navigate(['/login']); // Redirigir al login
     } catch (error: unknown) {
       if (error instanceof Error) {
-        alert(' Error en el registro: ' + error.message);
+        alert('❌ ' + error.message);
       } else {
-        alert(' Error desconocido en el registro');
+        alert('❌ Error desconocido en el registro');
       }
     }
-    
   }
 
   /**
